03712510f6d2dec2874c99892267ce2a3e1dd597
Kasey <koitvi4@gmail.com>
2025-05-20 13:37:28 +0500

    apdts from accademia

--- scripts/Player/UI/Player_UI_Screen.gd
+++ scripts/Player/UI/Player_UI_Screen.gd
@@ -9,12 +9,43 @@ var health_count = 20
 var money_count = 0
 var exp_count = 0
 
-
 func _process(_delta):
 	health.text = "health: " +str(health_count)
 	money.text = "money: " +str(money_count)
 	exp.text = "exp: " +str(exp_count)
 
-
 func _on_button_pressed() -> void:
 	save_manager.save_game()
+
+
+func save():
+	var save_dict = {
+		"health" : health_count,
+		"money" : money_count,
+		"exp" : exp_count,
+	}
+	return save_dict
+
+func save_game():
+	var file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	if file:
+		var json_string = JSON.stringify(save())
+		file.store_line(json_string)
+		file.close()
+		print("save game!")
+
+func load_game():
+	if not FileAccess.file_exists("user://savegame.save"):
+		print("Нет файла сохранения.")
+		return
+	var file = FileAccess.open("user://savegame.save", FileAccess.READ)
+	if file:
+		var json_string = file.get_line()
+		var json = JSON.new()
+		var parse_result = json.parse(json_string)
+		if parse_result != OK:
+			print("Ошибка парсинга JSON")
+			return
+		var data = json.get_data()
+		print("load game!")
+		file.close()
